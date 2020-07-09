a6810156e3aac5693386bf1f6c3db87956c4f60d
Baldeep Kang <bkang4612@gmail.com>
2020-07-09 12:51:34 -0700

    Add function for count down time

--- script.js
+++ script.js
@@ -8,32 +8,103 @@ var questions = [
       title: 'The condition in an if / else statement is enclosed within ____.',
       choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
       answer: 'parentheses'
+      
     }
   ];
 
+  var score = 0;
+  var time;
+  var questionNumber = 0;
+  var userChoice;
 
   function startQuiz(){
     var message = document.getElementById("message");
+    var quizSelection = document.getElementById("quizSelection");
 
     message.style.display = "block";
-
+    quizSelection.style.display = "none";
     if (message.style.display === "block"){
-      message.style.display = "none";
+        message.style.display = "none";
+        quizSelection.style.display = "block";
     }
     else{
         message.style.display = "block";
+        quizSelection.style.display = "none";
 
-    }
+   
 }
 
-// function quizQuestions() {
-//   var questionRow = document.getElementById("questionRow");
+$("#comQuest").on("click", function(){
+  countDown();
+  });
 
-//   if (questionRow.style.display === "block"){
-//     questionRow.style.display = "none";
-//   }
-//   else{
-//       message.style.display = "block";
+function quizQuestions() {
+  var questionRow = document.getElementById("questionRow");
 
-//   }
+  quizSelection.style.display = "block"
+  quizOption.style.display = "none";
+  if (quizSelection.style.display === "block"){
+    quizSelection.style.display = "none"
+    questionRow.style.display = "block";
+  }
+  else{
+    quizSelection.display = "block";
+      questionRow.style.display = "none";
+
+  }
+  
+
+  $("#title").children().hide();
+  $("#choices").children().hide();
+  $("#answer").children().hide();
+  // $("#sign").children().hide();
+  // $("#timer").children().hide();
+
+  var showQuestion = $("<h2>");
+  showQuestion.attr("id", questions[questionNumber].choices);
+  showQuestion.append(questions[questionNumber].title);
+  $("#title").append(showQuestion);
+
+  
+
+  for (var i = 0; i < 4; i++) {
+
+    var choiceList = $("<button>");
+    choiceList.attr("type", "button");
+    choiceList.attr("value", questions[questionNumber].choices[i]);
+    choiceList.css("margin-top", "50px");
+    choiceList.addClass("btn btn-secondary btn-lg btn-block choices");
+    choiceList.append(questions[questionNum].choices[i]);
+    $("#choices").append(choiceList);
+  }
+
+  function countDown(){
+    $("#timer").empty();
+    timer = 75;
+    var timeDiv = $("<h6>");
+    timeDiv.append("Time remaining: " + timer + "seconds");
+    $("#timer").append(timeDiv);
+    time = setInterval(clock, 1000);
+    
+    }
+    
+    function clock(){
+    $("#timer").empty();
+    timer--;
+    var timeDiv = $("<h6>");
+    timeDiv.append("Time remaining: " + timer + "seconds");
+    $("#timer").append(timeDiv);
+    
+    if(timer < 1){
+        $("#title").children().hide();
+        $("#choices").children().hide();
+        $("#answer").children().hide();
+        $("#sign").children().hide();
+        $("#timer").children().hide();
+    
+        setTimeout(finalPage, 1000);
+        clearInterval(time);
+
+
+  
 }
